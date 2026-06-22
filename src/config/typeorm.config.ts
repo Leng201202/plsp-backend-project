27dd21d466f9157be6694a08321afafbf2803afc
Leng201202 <6631503129@lamduan.mfu.ac.th>
2026-06-22 22:34:57 +0700

    Fix Database name

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,7 +7,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     : { host: process.env.DB_HOST, port: Number(process.env.DB_PORT || 3306) }),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE_NAME,
 
   autoLoadEntities: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
